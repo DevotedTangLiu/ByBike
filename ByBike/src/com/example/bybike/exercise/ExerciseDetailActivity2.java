c1d1771cc10f6c38c1b0c5107a844c2bca6c009a
devotedwife <tangliu@mail2.sysu.edu.cn>
2014-11-28 17:19:29 +0800

    添加shareSDK支持

--- ByBike/src/com/example/bybike/exercise/ExerciseDetailActivity2.java
+++ ByBike/src/com/example/bybike/exercise/ExerciseDetailActivity2.java
@@ -29,6 +29,8 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ZoomControls;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 import com.ab.activity.AbActivity;
 import com.ab.bitmap.AbImageDownloader;
@@ -77,6 +79,8 @@ public class ExerciseDetailActivity2 extends AbActivity {
 	TextView applyUserCount;
 	TextView discussCount;
 	Button discussButton;
+	
+	Button shareButton;
 
 	private ScalingRunnable mScalingRunnable;
 	private int mHeaderHeight;
@@ -341,6 +345,10 @@ public class ExerciseDetailActivity2 extends AbActivity {
 				hasHelmet.setSelected(true);
 			}
 			break;
+			
+		case R.id.shareButton:
+			showShare();
+			break;
 		default:
 			break;
 		}
@@ -528,6 +536,11 @@ public class ExerciseDetailActivity2 extends AbActivity {
 				.getLayoutParams();
 		llp.height = localLayoutParams.height;
 		spaceArea.setLayoutParams(llp);
+		
+		mMapView.requestLayout();
+		mMapView.invalidate();
+		
+
 	}
 
 	private static final Interpolator sInterpolator = new Interpolator() {
@@ -600,6 +613,36 @@ public class ExerciseDetailActivity2 extends AbActivity {
 				exercisePicArea.post(this);
 			}
 		}
+	}
+
+	private void showShare() {
+		ShareSDK.initSDK(this);
+		OnekeyShare oks = new OnekeyShare();
+		// 关闭sso授权
+		oks.disableSSOWhenAuthorize();
+
+		// 分享时Notification的图标和文字
+		oks.setNotification(R.drawable.ic_launcher,
+				getString(R.string.app_name));
+		// title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+		oks.setTitle(getString(R.string.share));
+		// titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+		oks.setTitleUrl("http://sharesdk.cn");
+		// text是分享文本，所有平台都需要这个字段
+		oks.setText("我是分享文本");
+		// imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+		oks.setImagePath("/sdcard/test.jpg");// 确保SDcard下面存在此张图片
+		// url仅在微信（包括好友和朋友圈）中使用
+		oks.setUrl("http://sharesdk.cn");
+		// comment是我对这条分享的评论，仅在人人网和QQ空间使用
+		oks.setComment("我是测试评论文本");
+		// site是分享此内容的网站名称，仅在QQ空间使用
+		oks.setSite(getString(R.string.app_name));
+		// siteUrl是分享此内容的网站地址，仅在QQ空间使用
+		oks.setSiteUrl("http://sharesdk.cn");
+
+		// 启动分享GUI
+		oks.show(this);
 	}
 
 }
