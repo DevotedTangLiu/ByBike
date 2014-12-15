cd9d24e3a49c8a92cf0e953b4aa5398486ef4224
devotedwife <tangliu@mail2.sysu.edu.cn>
2014-12-15 11:42:04 +0800

    推送接口
    推送接口初步完成

--- ByBike/src/com/example/bybike/pushnotification/PushMsgRealService.java
+++ ByBike/src/com/example/bybike/pushnotification/PushMsgRealService.java
@@ -1,4 +1,3 @@
-
 /*
  * @(#) PushMsgRealService.java
  * @Author:tangliu(mail) 2014-12-14
@@ -6,76 +5,190 @@
  */
 package com.example.bybike.pushnotification;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.os.Looper;
 import android.util.Log;
 
+import com.example.bybike.db.model.PostMessage;
+import com.example.bybike.util.Constant;
+import com.example.bybike.util.SharedPreferencesUtil;
+
 /**
-  * @author tangliu(mail) 2014-12-14
-  * @version 1.0
-  * @modifyed by tangliu(mail) description
-  * @Function 类功能说明
-  */
-public class PushMsgRealService extends Service{
-    
-    boolean flag;
-    String tag = "pushservice";
+ * @author tangliu(mail) 2014-12-14
+ * @version 1.0
+ * @modifyed by tangliu(mail) description
+ * @Function 类功能说明
+ */
+public class PushMsgRealService extends Service {
 
-    
-    /*
-      * @param intent
-      * @return
-      * @see android.app.Service#onBind(android.content.Intent)
-      */
-    @Override
-    public IBinder onBind(Intent intent) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	boolean flag;
+	String tag = "pushservice";
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        doing();
-        Log.i(tag, "Service is Created");
-    }
-    
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        // doing();
-        Log.i(tag, "Service is Started");
-        return 1;
+	// Android设备唯一标识
+	String deviceId;
+	// 通知栏消息
+	private int messageNotificationID = 1000;
+	private Notification messageNotification = null;
+	private NotificationManager messageNotificatioManager = null;
 
-    }
-    
-    /**
-     * doing(这里用一句话描述这个方法的作用)
-     */
-   public void doing() {
-       new Thread(new Runnable() {
-           @Override
-           public void run() {
-               Looper.prepare();
-               PushMsgService contact = new PushMsgService(PushMsgRealService.this);
-               contact.turnOn();
-               contact.contact();
-               Looper.loop();
-           }
-       }).start();
-   }
-   
-   
-   @Override
-   public void onDestroy() {
-       PushMsgService contact = new PushMsgService(PushMsgRealService.this);
-       contact.turnOff();
-       super.onDestroy();
-       Log.i(tag, "Service is Destroyed");
-   }
+	// 获取消息线程
+	private MessageThread messageThread = null;
 
-    
+	// 点击查看
+	private Intent messageIntent = null;
+	private PendingIntent messagePendingIntent = null;
+
+	/*
+	 * @param intent
+	 * 
+	 * @return
+	 * 
+	 * @see android.app.Service#onBind(android.content.Intent)
+	 */
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Log.i(tag, "Service is Created");
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// doing();
+		Log.i(tag, "Service is Started");
+
+		// 开启线程
+		messageThread = new MessageThread();
+		messageThread.isRunning = true;
+		messageThread.start();
+
+		return 1;
+
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.i(tag, "Service is Destroyed");
+	}
+
+	/**
+	 * 从服务器端获取消息
+	 * 
+	 */
+	class MessageThread extends Thread {
+
+		// 运行状态，下一步骤有大用
+		public boolean isRunning = true;
+
+		public void run() {
+			while (isRunning) {
+				try {
+					// 休息10分钟 600000
+					Thread.sleep(Integer.parseInt(getNoticeTime()));
+					String data = getPromotMessageFromServer();
+					// long id;
+					try {
+						JSONObject resultObj = new JSONObject(data);
+						String code = resultObj.getString("code");
+						if ("0".equals(code)) {
+							
+							String sessionId = resultObj.getString("jsessionid");
+							SharedPreferencesUtil.saveSharedPreferences_s(PushMsgRealService.this, Constant.SESSION, sessionId);
+
+							JSONArray messageArray = resultObj.getJSONArray("data");
+							for (int i = 0; i < messageArray.length(); i++) {
+								JSONObject message = messageArray.getJSONObject(i);
+								String title = message.getString("title");
+								String content = message.getString("content");
+								String id = message.getString("id");
+
+								PostMessage pm = new PostMessage(PushMsgRealService.this);
+								pm.sendMessage(messageNotificationID, title, content);
+							}
+
+						}
+
+					} catch (JSONException e) {
+						e.printStackTrace();
+						// Log.e("JSONException1", e.toString());
+					} catch (Exception e) {
+						e.printStackTrace();
+						// Log.e("JSONException2", e.toString());
+					}
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					// Log.e("JSONException3", e.toString());
+				}
+			}
+		}
+	}
+
+	private String noticeTime = "";
+
+	private String getNoticeTime() {
+		noticeTime = String.valueOf(Constant.NOTIFICATION_PUSH_INTERVAL);
+		return noticeTime;
+	}
+
+	/**
+	 * 获取优惠信息
+	 */
+	String requestURL = Constant.serverUrl + Constant.pushUrl;
+
+	public String getPromotMessageFromServer() {
+		String url = requestURL + "?checkTime=" + System.currentTimeMillis();
+		return javaHttpGet(url);
+	}
+
+	private String javaHttpGet(String url) {
+		try {
+			URL pathUrl = new URL(url); // 创建一个URL对象
+			HttpURLConnection urlConnect = (HttpURLConnection) pathUrl
+					.openConnection(); // 打开一个HttpURLConnection连接
+			urlConnect.setRequestProperty("Accept-Encoding", "gzip");
+			urlConnect.setRequestProperty("User-Agent",
+					"%E5%8E%A6%E9%97%A8%E8%88%AA%E7%A9%BAandroid");
+			urlConnect.setConnectTimeout(30000); // 设置连接超时时间
+			urlConnect.connect();
+			InputStreamReader in = new InputStreamReader(
+					urlConnect.getInputStream()); // 得到读取的内容
+			BufferedReader buffer = new BufferedReader(in); // 为输出创建BufferedReader
+			String inputLine = "";
+			String resultData = "";
+			while (((inputLine = buffer.readLine()) != null)) {
+				resultData += inputLine;
+			}
+			return resultData;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
 
 }
-
--- ByBike/src/com/example/bybike/pushnotification/PushMsgService.java
+++ ByBike/src/com/example/bybike/pushnotification/PushMsgService.java
@@ -9,7 +9,8 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import android.content.Context;
-import android.os.SystemClock;
+import android.os.Looper;
+import android.util.Log;
 
 import com.ab.http.AbHttpUtil;
 import com.ab.http.AbRequestParams;
@@ -19,120 +20,144 @@ import com.example.bybike.util.Constant;
 import com.example.bybike.util.SharedPreferencesUtil;
 
 /**
-  * @author tangliu(mail) 2014-12-14
-  * @version 1.0
-  * @modifyed by tangliu(mail) description
-  * @Function 类功能说明
-  */
+ * @author tangliu(mail) 2014-12-14
+ * @version 1.0
+ * @modifyed by tangliu(mail) description
+ * @Function 类功能说明
+ */
 public class PushMsgService {
 
-    private Context pushMsgRealService;
-    boolean flag = true;
+	private Context pushMsgRealService;
+	boolean flag = true;
 
-    // http请求帮助类
-    private AbHttpUtil mAbHttpUtil = null;
-    String requestURL;
+	// http请求帮助类
+	private AbHttpUtil mAbHttpUtil = null;
+	String requestURL;
 
-    /**
-     * 创建一个新的实例 
-     * @param pushMsgRealService
-     */
-    public PushMsgService(Context pushMsgRealService) {
-        this.pushMsgRealService = pushMsgRealService;
-        // 获取Http工具类
-        mAbHttpUtil = AbHttpUtil.getInstance(this.pushMsgRealService);
-        mAbHttpUtil.setDebug(false);
-        requestURL = Constant.serverUrl + Constant.pushUrl;
-    }
+	/**
+	 * 创建一个新的实例
+	 * 
+	 * @param pushMsgRealService
+	 */
+	public PushMsgService(Context pushMsgRealService) {
+		this.pushMsgRealService = pushMsgRealService;
+		// 获取Http工具类
+		mAbHttpUtil = AbHttpUtil.getInstance(this.pushMsgRealService);
+		mAbHttpUtil.setDebug(false);
+		requestURL = Constant.serverUrl + Constant.pushUrl;
+	}
 
-    /**
-     * contact(这里用一句话描述这个方法的作用)
-     */
-    public void contact() {
+	/**
+	 * contact(这里用一句话描述这个方法的作用)
+	 */
+	public void contact() {
 
-        AbRequestParams p = new AbRequestParams();
-        while (flag) {
-            SystemClock.sleep(Constant.NOTIFICATION_PUSH_INTERVAL);
-            p.put("checkTime", String.valueOf(System.currentTimeMillis()));
-            mAbHttpUtil.post(requestURL, p, listener);
-//            flag = false;
-        
-            
-        }
-    }
-    
-    /**
-     * 处理返回结果
-      * processResult(这里用一句话描述这个方法的作用)
-      * @param resultString
-     */
-    private void processResult(String resultString){
-        try{
-            JSONObject resultObj = new JSONObject(resultString);
-            String code = resultObj.getString("code");
-            if("0".equals(code)){
-                String sessionId = resultObj.getString("jsessionid");
-                SharedPreferencesUtil.saveSharedPreferences_s(this.pushMsgRealService, Constant.SESSION, sessionId);
-                
-                JSONArray messageArray = resultObj.getJSONArray("data");
-                for(int i = 0; i < messageArray.length(); i ++){
-                    JSONObject message = messageArray.getJSONObject(i);
-                    String title = message.getString("title");
-                    String content = message.getString("content");
-                    String id = message.getString("id");
-                    
-                    PostMessage pm = new PostMessage(pushMsgRealService);
-                    pm.sendMessage(0, title, content);
-                }
-            }
-        }catch(Exception e){
-            
-        }finally{
-//            SystemClock.sleep(Constant.NOTIFICATION_PUSH_INTERVAL);
-//            flag = true;
-        }
-    }
-    
-    AbStringHttpResponseListener listener = new AbStringHttpResponseListener() {
+		Log.v("3", Thread.currentThread().getName());
+		AbRequestParams p = new AbRequestParams();
+		if (flag) {
+			p.put("checkTime", String.valueOf(System.currentTimeMillis()));
+			mAbHttpUtil.post(requestURL, p, new AbStringHttpResponseListener() {
 
-        // 获取数据成功会调用这里
-        @Override
-        public void onSuccess(int statusCode, String content) {
-            
-            processResult(content);
-        };
+				// 获取数据成功会调用这里
+				@Override
+				public void onSuccess(int statusCode, String content) {
 
-        // 开始执行前
-        @Override
-        public void onStart() {
-        }
+					processResult(content);
+				};
 
-        // 失败，调用
-        @Override
-        public void onFailure(int statusCode, String content, Throwable error) {
-        }
+				// 开始执行前
+				@Override
+				public void onStart() {
+				}
 
-        // 完成后调用，失败，成功
-        @Override
-        public void onFinish() {
-        };
+				// 失败，调用
+				@Override
+				public void onFailure(int statusCode, String content,
+						Throwable error) {
+				}
 
-    };
+				// 完成后调用，失败，成功
+				@Override
+				public void onFinish() {
+				};
 
-    /**
-     * turnOn(这里用一句话描述这个方法的作用)
-     */
-    public void turnOn() {
-        flag = true;
+			});
+		}
+	}
 
-    }
+	/**
+	 * 处理返回结果 processResult(这里用一句话描述这个方法的作用)
+	 * 
+	 * @param resultString
+	 */
+	private void processResult(String resultString) {
+		try {
+			JSONObject resultObj = new JSONObject(resultString);
+			String code = resultObj.getString("code");
+			if ("0".equals(code)) {
+				String sessionId = resultObj.getString("jsessionid");
+				SharedPreferencesUtil.saveSharedPreferences_s(
+						this.pushMsgRealService, Constant.SESSION, sessionId);
 
-    /**
-      * turnOff(这里用一句话描述这个方法的作用)
-      */
-    public void turnOff() {
-        flag = false;
+				JSONArray messageArray = resultObj.getJSONArray("data");
+				for (int i = 0; i < messageArray.length(); i++) {
+					JSONObject message = messageArray.getJSONObject(i);
+					String title = message.getString("title");
+					String content = message.getString("content");
+					String id = message.getString("id");
 
-    }
+					PostMessage pm = new PostMessage(pushMsgRealService);
+					pm.sendMessage(0, title, content);
+				}
+			}
+		} catch (Exception e) {
+
+		} finally {
+			// SystemClock.sleep(Constant.NOTIFICATION_PUSH_INTERVAL);
+			// flag = true;
+		}
+	}
+
+	AbStringHttpResponseListener listener = new AbStringHttpResponseListener() {
+
+		// 获取数据成功会调用这里
+		@Override
+		public void onSuccess(int statusCode, String content) {
+
+			processResult(content);
+		};
+
+		// 开始执行前
+		@Override
+		public void onStart() {
+		}
+
+		// 失败，调用
+		@Override
+		public void onFailure(int statusCode, String content, Throwable error) {
+		}
+
+		// 完成后调用，失败，成功
+		@Override
+		public void onFinish() {
+		};
+
+	};
+
+	/**
+	 * turnOn(这里用一句话描述这个方法的作用)
+	 */
+	public void turnOn() {
+		flag = true;
+
+	}
+
+	/**
+	 * turnOff(这里用一句话描述这个方法的作用)
+	 */
+	public void turnOff() {
+		flag = false;
+
+	}
 
 }
